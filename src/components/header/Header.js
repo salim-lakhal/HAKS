3ac4a7e436167b530061cf7116e53b21d354961f
Salim <lakhalsalim@hotmail.com>
2023-12-23 19:27:16 +0100

    unused imports on header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './header.css';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import Logo from './logo.png'; 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
