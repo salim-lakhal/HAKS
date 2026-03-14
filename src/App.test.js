9b97bdb239a2baa3f082e8e36d966348424032df
Salim LAKHAL <salim.lakhal@telecom-sudparis.eu>
2026-03-14 16:46:36 +0100

    fix default test to match actual content

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headings = screen.getAllByText(/HAKS 2024/i);
+  expect(headings.length).toBeGreaterThan(0);
 });
